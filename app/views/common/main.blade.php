b9772ce7fc77af4442207dbbaea1c9d0b1c865af
Dan Brown <ssddanbrown@googlemail.com>
2014-03-04 21:41:35 +0000

    Created table styling for jobs

--- app/views/common/main.blade.php
+++ app/views/common/main.blade.php
@@ -6,6 +6,12 @@
 	{{ HTML::style('css/style.css') }}
 </head>
 <body>
+	<div class="row" id="header">
+		<div class="row-12">
+			{{ link_to_route('jobs.index', 'All Jobs') }}
+			{{ link_to_route('customers.index', 'All Customers') }}
+		</div>
+	</div>
 	<div class="row">
 		@yield('content')
 	</div>
--- app/views/jobs/index.blade.php
+++ app/views/jobs/index.blade.php
@@ -3,10 +3,34 @@
 @section('content')
 <div class="row-12">
 	<h1>All Jobs</h1>
+
 	@if ( count($jobs) >= 1 )
-		@foreach ( $jobs as $job )
-			<li>{{ link_to("/jobs/{$job->id}", $job->title ) }}</li>
-		@endforeach
+		<table class="table-grid" >
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>Job</th>
+					<th>Customer</th>
+					<th>Due date</th>
+					<th>Date Added</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach ( $jobs as $job )
+					<tr>
+						<td>{{ $job->id }}</td>
+						<td>{{ link_to_route('jobs.show', $job->title, $job->id) }}</td>
+						@if( $job->customer != null )
+							<td>{{ $job->customer->name }}</td>
+						@else
+							<td>No Customer</td>
+						@endif
+						<td>{{ date_create($job->due)->format('jS F, g:i a') }}</td>
+						<td>{{ date_create($job->created_at)->format('jS F, g:i a') }}</td>
+					</tr>
+				@endforeach
+			</tbody>
+		</table>
 	@else
 		<p>No jobs to display....</p>
 	@endif
