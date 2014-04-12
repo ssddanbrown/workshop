338a89656ef2d963eaa90feb9711673056fc906b
Dan Brown <ssddanbrown@googlemail.com>
2014-04-12 15:12:16 +0100

    Updated server sync script + moved files

--- serversync.sh
+++ serversync.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-rsync --exclude='.git/' --exclude='.sass-cache/' -azvv -e ssh ~/www/workshop/ workshop4@shell.gridhost.co.uk:~/ ;
+echo "Pulling from git server....";
+
+git pull;
+
+echo "Rebuilding css using compass";
+
+compass compile public;
