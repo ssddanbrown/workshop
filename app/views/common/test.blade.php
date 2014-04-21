f1254e20d66bf2366490264151dd6ab2595eef77
Dan Brown <ssddanbrown@googlemail.com>
2014-04-21 18:01:33 +0100

    Made date selection work for picker

--- app/views/common/test.blade.php
+++ app/views/common/test.blade.php
@@ -3,50 +3,126 @@
 @section('content')
 	
 <div id="datepicker">
-	<div class="datepicker-date"></div>
 </div>
+
 <script>
 $(document).ready(function(){
 
-	function repeat( string, num ){
-	    return new Array( num + 1 ).join( string );
-	}
+	function dateTimePicker(target){
 
-	var canvas = $('#datepicker');
-	var months = ['January', 'February', 'March', 'April', 'May', 'June',
+		this.canvas = target;
+
+		this.months = ['January', 'February', 'March', 'April', 'May', 'June',
 		'July', 'August', 'September', 'October', 'November', 'December'];
-	var days = ['SUN', 'MON', 'TUE', 'WED', 'THUR', 'FRI', 'SAT'];
-	var date = new Date();
-	var month = date.getMonth();
-	var year = date.getFullYear();
+		this.days = ['SUN', 'MON', 'TUE', 'WED', 'THUR', 'FRI', 'SAT'];
+		this.date = new Date(); // Change to get from input
+		this.month = this.date.getMonth();
+		this.year = this.date.getFullYear();
 
-	var html = '<div class="clear">';
-	html += '<div class="datepicker-date">MON</div>';
-	html += '<div class="datepicker-date">TUE</div>';
-	html += '<div class="datepicker-date">WED</div>';
-	html += '<div class="datepicker-date">THUR</div>';
-	html += '<div class="datepicker-date">FRI</div>';
-	html += '<div class="datepicker-date">SAT</div>';
-	html += '<div class="datepicker-date">SUN</div>';
-	html += '</div>';
+		this.set = function(){};
+		this.set.month = 3;
+		this.set.day = 20;
+		this.set.year = 2014;
 
-	var daysInMonth = new Date(year, month+1, 0).getDate();
+		this.monthRow = $(document.createElement('div'));
+		this.monthDisplay = $(document.createElement('span'));
+		this.datesArea = $(document.createElement('div'));
 
-	html += '<div id="datepicker-dates">';
-	for(var i=0; i<daysInMonth; i++){
-		if(i==0){
-			var dayInWeek = new Date(year, month+1, i).getDay();
-			if( dayInWeek != 1){
-				var diff = 6;
-				if( dayInWeek > 1){ diff = dayInWeek - 1; }
-				html += repeat('<div class="datepicker-date"></div>', diff);
+		this.repeat = function( string, num ){
+	    	return new Array( num + 1 ).join( string );
+		}
+
+		this.getSetDate = function(){
+			return new Date(this.set.year, this.set.month, this.set.day);
+		}
+
+		this.setup = function(){
+			var context = this;
+			this.monthRow.attr('class', 'clear datepicker-monthrow');
+			this.monthRow.append('<button type="button" class="datepicker-prev"><</button>');
+			this.monthRow.append(context.monthDisplay);
+			this.monthRow.append('<button type="button" class="datepicker-next">></button>');
+			this.canvas.append(context.monthRow);
+			var dayRow = document.createElement('div');
+			dayRow.className = 'clear';
+			dayRow.innerHTML = '<div class="datepicker-date">MON</div>'
+				+	'<div class="datepicker-date">TUE</div>'
+				+	'<div class="datepicker-date">WED</div>'
+				+	'<div class="datepicker-date">THUR</div>'
+				+	'<div class="datepicker-date">FRI</div>'
+				+	'<div class="datepicker-date">SAT</div>'
+				+	'<div class="datepicker-date">SUN</div>';
+			this.canvas.append(dayRow);
+
+			this.datesArea.attr('class', 'datepicker-dates clear');
+			this.canvas.append(this.datesArea);
+
+			// Show dates for current month
+			this.refreshDates();
+
+			// Click Events
+			this.canvas.on('click', '.datepicker-next', function(){
+				context.nextMonth();
+			});
+			this.canvas.on('click', '.datepicker-prev', function(){
+				context.previousMonth();
+			});
+			this.canvas.on('click', 'button.datepicker-date', function(){
+				context.set.year = context.year;
+				context.set.month = context.month;
+				context.set.day = $(this).data('day');
+				context.canvas.find('.current').removeClass('current');
+				$(this).addClass('current');
+			});
+		}
+
+		this.nextMonth = function(){
+			if ( this.month < 11 ) {
+				this.month ++;
+			} else {
+				this.year ++;
+				this.month = 0;
+			};
+			this.refreshDates();
+		}
+		this.previousMonth = function(){
+			if( this.month == 0 ){
+				this.year --;
+				this.month = 11;
+			} else {
+				this.month--;
+			}
+			this.refreshDates();
+		}
+
+		this.refreshDates = function(){
+			var context = this;
+			this.monthDisplay.html(context.months[context.month] + ' - ' + context.year);
+			this.datesArea.empty();
+			var daysInMonth = new Date(this.year, this.month + 1, 0).getDate();
+			for(var i = 0; i<daysInMonth; i++){
+				if(i==0){
+					var dayInWeek = new Date(this.year, this.month, i+1).getDay();
+					if( dayInWeek != 1){
+						var diff = 6;
+						if( dayInWeek > 1){ diff = dayInWeek - 1; }
+						this.datesArea.html(this.repeat('<div class="datepicker-date"></div>', diff));
+					}
+				}
+				if( this.set.year == this.year && this.set.month == this.month && this.set.day == i+1 ){
+					this.datesArea.append('<button type="button" data-day="'+ (i+1) +'" class="datepicker-date current">' + (i+1) +'</button>');
+				} else {
+					this.datesArea.append('<button type="button" data-day="'+ (i+1) +'" class="datepicker-date">' + (i+1) +'</button>');
+				}
 			}
 		}
-		html += '<div class="datepicker-date">' + (i+1) +'</div>';
-	}
-	html += '</div>';
 
-	canvas.html(html);
+
+	}
+
+	var instance = new dateTimePicker( $('#datepicker') );
+	instance.setup();
+
 });
 </script>
 
