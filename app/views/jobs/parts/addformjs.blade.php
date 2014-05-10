590e32d4b23b4d02fcc8f4f8d3179d2c19f233b1
Dan Brown <ssddanbrown@googlemail.com>
2014-05-10 14:40:23 +0100

    Commented js for tables

--- app/views/jobs/parts/addformjs.blade.php
+++ app/views/jobs/parts/addformjs.blade.php
@@ -5,30 +5,33 @@ $(document).ready(function(){
 		var biggestIndex = 0;
 		var model = container.find('.model').first();
 
+		// Finds largest data id to ensure each row is unique
 		var containerChildren = container.children('.table-row');
 		containerChildren.each(function(){
 			var index = $(this).data('index');
 			index = parseInt(index);
 			if(index >= biggestIndex) biggestIndex = index+1;
 		});
+
+		// Create new row and change propeties
 		var newRow = model.clone().removeAttr('style');
 		newRow.attr('data-index', biggestIndex);
-
 		newRow.find('input').each(function(){
 			var name = $(this).attr('name');
 			$(this).attr( 'name', name.replace('[-1]', '['+biggestIndex+']') );
 		});
-
+		// Attached new row to container
 		container.append(newRow);
 	}
 
+	// Form Button Clicks
 	$('#button-add-item').click(function(){
 		addForm( $('#item-container') );
 	});
 	$('#button-add-cost').click(function(){
 		addForm( $('#cost-container') );
 	});
-
+	// Table Row deletion
 	$('table').on('click', '.delete-row', function(){
 		var row = $(this).parent().parent();
 		row.find('input').val('');
@@ -37,6 +40,6 @@ $(document).ready(function(){
 		}
 	});
 
-
+// Jquery End
 });
 </script>
