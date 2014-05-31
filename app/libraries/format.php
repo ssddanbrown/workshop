2b81296aa1e34af8b6f3e5e4a671cd8265732815
Dan Brown <ssddanbrown@googlemail.com>
2014-05-31 22:25:21 +0100

    Cleaned formatter class

--- app/libraries/format.php
+++ app/libraries/format.php
@@ -1,14 +1,5 @@
 <?php
 
-function _n( $single, $plural, $var)
-{
-	if ($var == 1) {
-		return $single;
-	} else {
-		return $plural;
-	}
-}
-
 Class Format {
 
 	/**
@@ -22,6 +13,11 @@ Class Format {
 		return $currency . sprintf('%0.2f', $price);
 	}
 
+	/**
+	 * Date - Format dates for user viewing
+	 * @param  Integer $timestamp Timestamp or datetime string
+	 * @return String             Formatted date string
+	 */
 	public static function date( $timestamp = 'now' )
 	{
 		if( is_int($timestamp) ){
@@ -34,6 +30,11 @@ Class Format {
 		return $timestamp->format($format);
 	}
 
+	/**
+	 * Date To Database - Change user readable date string to valid database format
+	 * @param  String $dateString Input date string in valid format 'd-m-Y H:i'
+	 * @return String             Formatted timestamp for database
+	 */
 	public static function dateToDatabase($dateString)
 	{
 		$datetime = date_create_from_format('d-m-Y H:i', $dateString);
@@ -43,6 +44,12 @@ Class Format {
 		return $datetime->format('Y-m-d H:i:s');
 	}
 	
+	/**
+	 * Human Time - Converts date string/int to readable relative time
+	 * @param  Date String/Int $from 
+	 * @param  Date String/Int $to   
+	 * @return String       Readable time difference
+	 */
 	public static function humanTime( $from, $to = '' )
 	{
 		if ( empty( $to ) ) $to = time();
@@ -50,9 +57,9 @@ Class Format {
 		$from = date_create($from)->getTimestamp();
 
 		if ($from <= $to) {
-			$extra = ' Ago';
+			$extra = ' ago';
 		} else {
-			$extra = ' Away';
+			$extra = ' away';
 		}
 
 		$diff = (int) abs( $to - $from );
@@ -65,37 +72,36 @@ Class Format {
 
 		if ( $diff < $HOUR_IN_SECONDS ) {
 			$mins = round( $diff / $MINUTE_IN_SECONDS );
-			if ( $mins <= 1 )
-				$mins = 1;
-			/* translators: min=minute */
-			$since = sprintf( _n( '%s min', '%s mins', $mins ), $mins );
+			if ( $mins <= 1 ) $mins = 1;
+			$since = sprintf( self::checkPlural( '%s min', '%s mins', $mins ), $mins );
 		} elseif ( $diff < $DAY_IN_SECONDS && $diff >= $HOUR_IN_SECONDS ) {
 			$hours = round( $diff / $HOUR_IN_SECONDS );
-			if ( $hours <= 1 )
-				$hours = 1;
-			$since = sprintf( _n( '%s hour', '%s hours', $hours ), $hours );
+			if ( $hours <= 1 ) $hours = 1;
+			$since = sprintf( self::checkPlural( '%s hour', '%s hours', $hours ), $hours );
 		} elseif ( $diff < $WEEK_IN_SECONDS && $diff >= $DAY_IN_SECONDS ) {
 			$days = round( $diff / $DAY_IN_SECONDS );
-			if ( $days <= 1 )
-				$days = 1;
-			$since = sprintf( _n( '%s day', '%s days', $days ), $days );
+			if ( $days <= 1 ) $days = 1;
+			$since = sprintf( self::checkPlural( '%s day', '%s days', $days ), $days );
 		} elseif ( $diff < 30 * $DAY_IN_SECONDS && $diff >= $WEEK_IN_SECONDS ) {
 			$weeks = round( $diff / $WEEK_IN_SECONDS );
-			if ( $weeks <= 1 )
-				$weeks = 1;
-			$since = sprintf( _n( '%s week', '%s weeks', $weeks ), $weeks );
+			if ( $weeks <= 1 ) $weeks = 1;
+			$since = sprintf( self::checkPlural( '%s week', '%s weeks', $weeks ), $weeks );
 		} elseif ( $diff < $YEAR_IN_SECONDS && $diff >= 30 * $DAY_IN_SECONDS ) {
 			$months = round( $diff / ( 30 * $DAY_IN_SECONDS ) );
-			if ( $months <= 1 )
-				$months = 1;
-			$since = sprintf( _n( '%s month', '%s months', $months ), $months );
+			if ( $months <= 1 ) $months = 1;
+			$since = sprintf( self::checkPlural( '%s month', '%s months', $months ), $months );
 		} elseif ( $diff >= $YEAR_IN_SECONDS ) {
 			$years = round( $diff / $YEAR_IN_SECONDS );
-			if ( $years <= 1 )
-				$years = 1;
-			$since = sprintf( _n( '%s year', '%s years', $years ), $years );
+			if ( $years <= 1 ) $years = 1;
+			$since = sprintf( self::checkPlural( '%s year', '%s years', $years ), $years );
 		}
 
 		return $since . $extra;
-		}
+	}
+
+	static function checkPlural( $single, $plural, $var)
+	{
+		return $var == 1 ? $single : $plural;
+	}
+
 }
