ecba336d29ce3a041299fb94d34908ceebc09f2b
Dan Brown <ssddanbrown@googlemail.com>
2014-05-31 12:15:20 +0100

    Started basic header styling

--- app/views/common/main.blade.php
+++ app/views/common/main.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
 	<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,400italic,500,700' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Lily+Script+One' rel='stylesheet' type='text/css'>
 	<meta charset="UTF-8">
 	<title>Workshop</title>
 	{{ HTML::style('css/style.css') }}
@@ -10,14 +11,15 @@
 </head>
 <body>
 	
-	<!-- Header -->
-	<div class="row" id="header">
-			{{ link_to_route('jobs.index', 'All Jobs', null, array('class'=>'link')) }}
-			{{ link_to_route('customers.index', 'All Customers', null, array('class'=>'link')) }}
-			{{ link_to_route('templates.index', 'All Templates', null, array('class'=>'link')) }}
+	<div class="row" id="site-header">
+		<div class="logo">Workshop</div>
+		<ul class="inline">
+			<li>{{ link_to_route('jobs.index', 'Jobs') }}</li>
+			<li>{{ link_to_route('customers.index', 'Customers') }}</li>
+			<li>{{ link_to_route('templates.index', 'Templates') }}</li>
+		</ul>
 	</div>
 
-	<!-- Content -->
 	<div class="row">
 		@yield('content')
 	</div>
