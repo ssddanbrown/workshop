f3e18959f0b386e8cad6ba697ad2173c3678cd5d
Dan Brown <ssddanbrown@googlemail.com>
2014-07-19 21:12:54 +0100

    public template view fix

--- app/views/public/index.blade.php
+++ app/views/public/index.blade.php
@@ -4,16 +4,20 @@
 
 	<h1>Available Jobs</h1>
 	
-	@if(count($templates) > 0)
-		@foreach($templates as $template)
-			<div class="third">
-				<h4>{{ $template->title }}</h4>
-				<p>{{ $template->text }}</p>
-				<p><strong>{{ Format::price($template->total) }}</strong></p>
-			</div>
-		@endforeach
-	@else
-		<p>There are no jobs available at the moment</p>
-	@endif
+	<section>
 
+		@if(count($templates) > 0)
+			@foreach($templates as $template)
+				<div class="third">
+					<h4>{{ $template->title }}</h4>
+					<p>{{ $template->text }}</p>
+					<p><strong>{{ Format::price($template->total) }}</strong></p>
+				</div>
+			@endforeach
+		@else
+			<p>There are no jobs available at the moment</p>
+		@endif
+
+	</section>
+	
 @stop
